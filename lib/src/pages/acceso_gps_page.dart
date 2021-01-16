f88ebb5f52181e4a8ad6e29f8c4cb5c346f66eca
Vid <valdajosevid@gmail.com>
2021-01-15 20:06:10 -0400

    Start

---
+++ lib/src/pages/acceso_gps_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AccesoGPSPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('acceso'),
+      ),
+    );
+  }
+}
